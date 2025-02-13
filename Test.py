4778107a08e744e00697c8e23fc7c322a7592141
Baconzilla <105701413+Baconzilla-0@users.noreply.github.com>
2025-02-13 20:44:17 +1100

    fix test

--- Test.py
+++ Test.py
@@ -4,18 +4,15 @@ from pygame import Color, Rect, Vector2
 import UILibrary
 import UILibrary.Helpers as Helpers
 import UILibrary.Style as Style
-import UILibrary.Grid as Grid
-from UILibrary.Grid import Compass, Constraints, Divisions
-
-pygame.init()
+from UILibrary.Grid import *
 
 Sheet = Style.Sheet()
-Sheet.Entry(TargetClass="Any", State="Any").Set("Background", Color(140, 140, 140)).Set("Padding", Rect(5, 5, 5, 5)).Set("Margin", Rect(5,5,5,5))
-Sheet.Entry(TargetClass="Screen", State="Any").Set("Background", Color(140, 140, 140)).Set("Padding", Rect(0, 0, 0, 0)).Set("Margin", Rect(0,0,0,0))
+Sheet.Entry(TargetClass="Any", State="Any").Set("Background", Color(140, 140, 140)).Set("Padding", Rect(5, 5, 5, 5)).Set("Margin", Rect(5, 5, 5, 5))
+Sheet.Entry(TargetClass="Screen", State="Any").Set("Background", Color(140, 140, 140)).Set("Padding", Rect(0, 0, 0, 0)).Set("Margin", Rect(0, 0, 0, 0))
 
-Sheet.Entry(TargetClass="TextButton", State="Idle").Set("Background", Color(160, 160, 160)).Set("Margin", Rect(5,5,5,5))
-Sheet.Entry(TargetClass="TextButton", State="Hover").Set("Background", Color(180, 180, 180)).Set("Margin", Rect(5,5,5,5))
-Sheet.Entry(TargetClass="TextButton", State="Held").Set("Background", Color(130, 130, 130)).Set("Margin", Rect(5,5,5,5))
+Sheet.Entry(TargetClass="TextButton", State="Idle").Set("Background", Color(160, 160, 160)).Set("Margin", Rect(5, 5, 5, 5))
+Sheet.Entry(TargetClass="TextButton", State="Hover").Set("Background", Color(180, 180, 180)).Set("Margin", Rect(5, 5, 5, 5))
+Sheet.Entry(TargetClass="TextButton", State="Held").Set("Background", Color(130, 130, 130)).Set("Margin", Rect(5, 5, 5, 5))
 
 Text = 0.6
 
